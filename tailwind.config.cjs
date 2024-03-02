9376eeab08802461e24a8c2f354fb749af9081b5
Mariafaroe <132553150+Mariafaroe@users.noreply.github.com>
2024-03-02 17:12:04 +0100

    ALT

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,4 +7,40 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
     ],
+    theme: {
+        colors: {
+          transparent: "transparent",
+          current: "currentColor",
+          beige: " #dfdbd0",
+          grøn: "#313F32",
+          lightpink: "#f8afd7",
+          orange: "#ff784f",
+          darkblue: "#18165a",
+          lightblue: "#e9f4ff",
+          white: "#ffffff",
+          darkbeige:"#d1c4a7",
+        },
+        extend: {
+          fontFamily: {
+            poppins: ["Poppins", "sans-serif"],
+          },
+       
+        },
+        extend: {
+            fontFamily: {
+              impact: ["Impact", "sans-serif"],
+            },
+          },
+        fontSize: {
+            '10xl': '18rem',
+            '10xltele': '8rem',
+            '5xltele': '2rem',
+            'xl': '1.5rem',
+
+
+            
+          },
+       
+             
+      },
   };
